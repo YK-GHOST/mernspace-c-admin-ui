f2e13559616d940dd7edc4673cb6dfc38aed34e7
YK-GHOST <yogeshkantiwal20@gmail.com>
2025-04-30 17:44:29 +0530

    add Layouts

---
+++ src/layouts/GeneralLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const GeneralLayout = () => {
+  return (
+    <div>
+      GeneralLayout
+      <Outlet />
+    </div>
+  );
+};
+
+export default GeneralLayout;
--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/Login/LoginPage";
+import Dashboard from "./layouts/Dashboard";
+import GeneralLayout from "./layouts/GeneralLayout";
+import CategoryPage from "./pages/CategoryPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    index: true,
-    element: <HomePage />,
+    element: <Dashboard />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
   },
   {
-    path: "/auth/login",
-    element: <LoginPage />,
+    path: "/auth",
+    element: <GeneralLayout />,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+    ],
   },
 ]);
