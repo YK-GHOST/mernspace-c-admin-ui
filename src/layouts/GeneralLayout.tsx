9fba2c75d4bde3331cbbeaac725e358fe2be7312
YK-GHOST <yogeshkantiwal20@gmail.com>
2025-05-01 17:21:57 +0530

    add protected layout

--- src/layouts/GeneralLayout.tsx
+++ src/layouts/GeneralLayout.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
 
 const GeneralLayout = () => {
+  const { user } = useAuthStore();
+
+  if (user !== null) {
+    return <Navigate to="/" replace />;
+  }
   return (
     <div>
       GeneralLayout
