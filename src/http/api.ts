bc479767498ef6601b08137244c0ed436ea0824c
YK-GHOST <yogeshkantiwal20@gmail.com>
2025-05-15 21:32:15 +0530

    add auto refresh token

--- src/http/api.ts
+++ src/http/api.ts
@@ -4,7 +4,5 @@ import { api } from "./client";
 //Auth Service
 export const login = (credentials: Credentials) =>
   api.post("/auth/login", credentials);
-
 export const logout = () => api.post("/auth/logout");
-
 export const self = () => api.get("/auth/self");
--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
@@ -8,3 +9,30 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+const refreshToken = async () => {
+  await axios.get(`${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`, {
+    withCredentials: true,
+  });
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._isRetry) {
+      originalRequest._isRetry = true;
+      try {
+        const headers = { ...originalRequest.headers };
+        await refreshToken();
+        return api.request({ ...originalRequest, headers });
+      } catch (err) {
+        console.error("Token refresh error", err);
+        useAuthStore.getState().logout();
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
